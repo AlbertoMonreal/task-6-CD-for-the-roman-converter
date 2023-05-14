f52d578f04471aa731c1bedf5559d437aef6c1e6
Alberto Gonzalez <75039535+AlbertoMonreal@users.noreply.github.com>
2023-05-14 13:31:50 +0200

    Update tests.js

--- tests.js
+++ tests.js
@@ -6,7 +6,7 @@ test("Valid arguments", function(assert) {
 });
 
 test("Negative number", function (assert) {
-  assert.propEqual(convertIntegerToRoman("-189"), {value: 0, message: 'Please enter a valid integer', result: false}, "TC-5");
+  assert.propEqual(convertIntegerToRoman("-189"), {value: "0", message: 'Please enter a valid integer', result: false}, "TC-5");
 });
 
 test("Not exist roman number", function (assert) {
@@ -15,14 +15,14 @@ test("Not exist roman number", function (assert) {
 });
 
 test("Out of range", function (assert) {
- assert.propEqual(convertIntegerToRoman("4000"), {value: 0, message: 'Please enter a valid integer', result: false}, "TC-8");
+ assert.propEqual(convertIntegerToRoman("4000"), {value: "0", message: 'Please enter a valid integer', result: false}, "TC-8");
 });
 
 test("Invalid symbols", function (assert) {
- assert.propEqual(convertIntegerToRoman("1.23"), {value: 0, message: 'Please enter a valid integer', result: false}, "TC-9");
- assert.propEqual(convertIntegerToRoman("V"), {value: 0, message: 'Please enter a valid integer', result: false}, "TC-10");
+ assert.propEqual(convertIntegerToRoman("1.23"), {value: "0", message: 'Please enter a valid integer', result: false}, "TC-9");
+ assert.propEqual(convertIntegerToRoman("V"), {value: "0", message: 'Please enter a valid integer', result: false}, "TC-10");
  assert.propEqual(convertRomanToInteger("123"), {value: 0, message: 'Please enter a valid roman', result: false}, "TC-11");
- assert.propEqual(convertIntegerToRoman("0"), {value: 0, message: 'Please enter a valid integer', result: false}, "TC-12");
+ assert.propEqual(convertIntegerToRoman("0"), {value: "0", message: 'Please enter a valid integer', result: false}, "TC-12");
 });
 test("Empty", function (assert) {
  assert.propEqual(convertRomanToInteger(""), {value: 0, message: 'Please enter a valid roman', result: false}, "TC-13");
